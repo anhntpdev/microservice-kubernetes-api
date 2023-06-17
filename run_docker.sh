1b8c948a4d01792a3af66b1f30e6f5b9ec1d657a
anhnguyen <107665555+anhntpdev@users.noreply.github.com>
2023-06-17 12:24:05 +0700

    Added run_docker file

---
+++ run_docker.sh
@@ -0,0 +1,13 @@
+!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+docker build -t anhntpdev/project-ml-microservice-api .
+# Step 2: 
+# List docker images
+docker images
+# Step 3: 
+# Run flask app
+docker run --name microservices-api -d -p 8000:80 anhntpdev/project-ml-microservice-api
