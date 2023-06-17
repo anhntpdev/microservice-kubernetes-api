dfedbb54dd897dd432f57a05ec6b6e76ee250950
anhnguyen <107665555+anhntpdev@users.noreply.github.com>
2023-06-17 12:48:47 +0700

    Updated app.py

--- app.py
+++ app.py
@@ -1,32 +1,26 @@
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
-from flask import request
-from flask import jsonify
-from sensible.loginit import logger
-
-
-log = logger(__name__)
-#import logging
+import logging
 
 import pandas as pd
 from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
-# LOG = create_logger(app)
-# LOG.setLevel(logging.INFO)
+LOG = create_logger(app)
+LOG.setLevel(logging.INFO)
 
 def scale(payload):
     """Scales Payload"""
-    log.info("Start...")
-    log.info("Scaling Payload: \n{payload}")
+    
+    LOG.info(f"Scaling Payload: \n{payload}")
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home</h3>"
+    html = f"<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
@@ -61,15 +55,15 @@ def predict():
     
     # Logging the input payload
     json_payload = request.json
-    log.info("JSON payload: \n{json_payload}")
+    LOG.info(f"JSON payload: \n{json_payload}")
     inference_payload = pd.DataFrame(json_payload)
-    log.info("Inference payload DataFrame: \n{inference_payload}")
+    LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
     # scale the input
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
-    log.info("Prediction output: \n{prediction}")
+    LOG.info(f"Prediction output: \n{prediction}")
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
