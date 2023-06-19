f017e8eeaf50683ff768d0dad94338cefdff82cf
anhnguyen <107665555+anhntpdev@users.noreply.github.com>
2023-06-19 16:55:58 +0700

    fix Config.yml file

--- run_docker.sh
+++ run_docker.sh
@@ -4,10 +4,10 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t anhntp/microservice-api .
+docker build -t anhntp/project-ml-microservice-api .
 # Step 2: 
 # List docker images
 docker images
 # Step 3: 
 # Run flask app
-docker run --name microservices-api -d -p 8082:80 anhntp/microservice-api
+docker run --name microservices-api -d -p 8082:80 anhntp/project-ml-microservice-api
